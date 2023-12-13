7d891be1eb23ae04fc7ffc30b86c34a951dd342f
chomchom13 <chawhannaman5@gmail.com>
2023-12-14 03:02:15 +0530

    created middleware and a function to handle possible error

--- server/index.js
+++ server/index.js
@@ -17,6 +17,17 @@ app.use(express.json());
 app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter);
 
+// middleware for handling errors // next means the next middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.json({
+        success: false,
+        statusCode, // we can also write statusCode: statusCode, here but both have same names in this example so it works
+        message
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("Server is runnig on port 3000!")
 })
---
+++ server/utils/error.js
@@ -0,0 +1,8 @@
+// My errors -> for handling some errors which are not exactly an error ( for ex -> password string cannot be longer than 10 )
+
+export default errorHandler = (statusCode, message) =>{
+    const error = new Error(); // javascript error object
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+}
