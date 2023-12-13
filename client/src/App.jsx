5755e26143bc70782f7bcbaeebaecec9014e4af9
Naman Chawhan <chawhannaman5@gmail.com>
2023-12-13 09:39:08 +0530

    restruct

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/header";
+
+
+export default function App() {
+  return <BrowserRouter>
+  {/* putting the Header component here makes sure that this component will be present in all the routes that we are going to create below */}
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { FaSearch } from "react-icons/fa";
+{
+  /*fa -> font awesome FaSearch -> an icon from react-icons library*/
+}
+import { Link } from "react-router-dom";
+{
+  /* Link is used to navigate to different pages or routes without refreshing the page*/
+}
+
+export default function Header() {
+  return (
+    <header className='bg-slate-300 shadow-md'>
+        {/*justify-between -> adds a space between tags( if there are multiple tags )*/}
+        {/*items-center -> vertically centers the tags inside div*/}
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+                <h1 className='font-bold text-lg sm:text-xl flex flex-wrap'>
+                    {/*span is used when we want to apply style to specific texts inside a block of many texts*/}
+                    <span className='text-slate-500'>ChomChom</span>
+                    <span className='text-slate-700'>Estate</span>
+                </h1>
+            </Link>
+            <form className='bg-slate-200 p-2 rounded-lg flex items-center'>
+                {/*focus:outline-none -> removes the outline that appears when clicking the input field*/}
+                <input type="text " placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-500'/>
+            </form>
+            <ul className='flex gap-4'>
+                {/* hidden sm:inline -> hidden by default but for sm and above -> inline */}
+                <Link to='/'>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                </Link>
+                <Link to='/about'>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                </Link>
+                <Link to='/sign-in'>
+                <li className=' text-slate-700 hover:underline'>Sign in</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  );
+}
