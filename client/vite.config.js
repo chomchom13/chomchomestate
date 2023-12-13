08dac98eaadd9583dc72a3d816a17320cd482a31
chomchom13 <chawhannaman5@gmail.com>
2023-12-14 05:23:09 +0530

    completed sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/server': { // this means each time we write '/api' , take it as the target that we defined below this
+        target : 'http://localhost:3000',
+        secure : false,
+      }
+    }
+  }, 
   plugins: [react()],
 })
