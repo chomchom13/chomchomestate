94e13e696b8e739f57f2a38d7756155ca41ee95c
chomchom13 <chawhannaman5@gmail.com>
2023-12-15 00:47:26 +0530

    created sign in server route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +11,21 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json("User created successfully");
   } catch (error) {
-    next(error); // calls the middleware in the index.js file 
+    next(error); // calls the middleware in the index.js file
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email , password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email}); // we can also write email only as both have same names
+    if(!validUser) return next(errorHandler(404, "User not found!")); // our custom made errorHandler from utils folder
+    const validPassword = bcryptjs.compareSync(password, validUser.password) // compareSync method of bcryptjs automatically compares the password that the user just inputed with the hashed password in the database if there is any
+    if(!validPassword) return next(errorHandler(401, "Wrong Credentials!"))
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET) // JWT_SECRET key is unique and is only known to us . This unique key only belongs to our application 
+    const { password: pass, ...rest } = validUser._doc; // Here , we don't want to send the password to the user as a response so we are seperating the rest of the validUser data with the password and then we will send the rest ( which is rest of the user info except the password ( the password exists in validUser._doc ))
+    res.cookie('access_token',token, {httpOnly : true}).status(200).json(rest); // httpOnly: true adds security to out cookie and ensures that the cookie belongs to our application
+  } catch (error) {
+    next(error); // calls the middleware in the index.js file
   }
 };
